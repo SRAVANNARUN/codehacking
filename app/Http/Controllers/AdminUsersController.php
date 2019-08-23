6235e3c0fdd2cba180a81df9c06e7fdfa97165fb
srunvannara <srunvannara@gmail.com>
2019-08-23 11:24:23 +0700

    user with photo upload feature

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 
 use App\Http\Requests\UsersRequest;
+use App\Photo;
 use App\Role;
 use App\User;
 use Illuminate\Http\Request;
@@ -41,9 +42,17 @@ class AdminUsersController extends Controller
      */
     public function store(UsersRequest $request)
     {
-        User::create($request->all());
-        return redirect('/admin/users/index');
-//        return $request->all();
+        $input=$request->all();
+
+        if($file=$request->file('photo_id')){
+            $name=time().$file->getClientOriginalName();
+            $file->move('images', $name);
+            $photo=Photo::create(['file'=>$name]);
+            $input['photo_id']=$photo->id;
+        }
+        $input['password']=bcrypt($request->password);
+        User::create($input);
+        return redirect('/admin/users');
     }
 
     /**
@@ -54,7 +63,7 @@ class AdminUsersController extends Controller
      */
     public function show($id)
     {
-        return view('admin.users.show');
+        //
     }
 
     /**
